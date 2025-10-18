b0c459469e47ed4b621b6a1bd30ab8723cdf6289
Jasmine <oatman.estates.holdings@gmail.com>
2025-10-18 08:49:59 -0400

    updates

---
+++ src/pages/Home.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import { useNavigate } from 'react-router-dom';
+
+const Home =() => {
+    const navigate = useNavigate();
+    const [users, setUsers] = useState([])
+
+    async function getUsers() {
+        const { data } = await axios.get(
+            "https://jsonplaceholder.typicode.com/users/"
+        );
+        setUsers(data);
+        console.log(data);
+    }
+
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+return (
+    <div className="container">
+  <div className="row">
+    <div className="user-list">
+    {users.map((user) =>(
+    <div className="user" key={user.id}>
+    <div className='user-card' onClick={()=>navigate(`${user.id}`)}>
+        <div className='user-card__container'>
+            <h3>{user.name}</h3>
+            <p>
+                <b>Email:</b> {user.email}
+            </p>
+            <p>
+                <b>Phone:</b> {user.phone}
+            </p>
+            <p>
+                <b>Website:</b> {user.website}
+            </p>
+        </div>
+    </div>
+    </div>
+))}
+    </div>
+  </div>
+</div>
+  );
+};
+
+
+export default Home;
